88c3ab58653c20b3f19f2f7e91949a6acc1e00d9
AyaHamed <aya.hamed181@gmail.com>
2023-01-15 17:25:14 +0400

    prov reviews

--- lib/provider/screens/prov_home/tabs/prov_reviews/ProvReviews.dart
+++ lib/provider/screens/prov_home/tabs/prov_reviews/ProvReviews.dart
@@ -12,9 +12,62 @@ class _ProvReviewsState extends State<ProvReviews> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const DefaultAppBar(
-        title: "Reviews",
-        showLeading: false,
+      backgroundColor: MyColors.bg,
+      appBar: const DefaultAppBar(title: "Reviews"),
+      body: Container(
+        margin: const EdgeInsets.all(10),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+        decoration: BoxDecoration(
+          color: MyColors.white,
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+              color: MyColors.greyWhite,
+              spreadRadius: 1,
+              blurRadius: 5,
+            ),
+          ],
+        ),
+        child: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 10),
+              child: RatingBar.builder(
+                initialRating: 2,
+                minRating: 0,
+                direction: Axis.horizontal,
+                allowHalfRating: false,
+                updateOnDrag: false,
+                itemCount: 5,
+                itemSize: 30,
+                itemPadding: const EdgeInsets.symmetric(horizontal: 1.0),
+                itemBuilder: (context, _) => const Icon(
+                  Icons.star,
+                  color: Colors.amber,
+                ),
+                ignoreGestures: true,
+                onRatingUpdate: (index) {},
+              ),
+            ),
+            Flexible(
+              child: AnimationLimiter(
+                child: ListView.builder(
+                  itemCount: 10,
+                  itemBuilder: (BuildContext context, int index) {
+                    return AnimationConfiguration.staggeredList(
+                      position: index,
+                      duration: const Duration(milliseconds: 375),
+                      child: const SlideAnimation(
+                        verticalOffset: 50.0,
+                        child: BuildDoctorReviewItem(),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/provider/screens/prov_home/tabs/prov_reviews/ProvReviewsImports.dart
+++ lib/provider/screens/prov_home/tabs/prov_reviews/ProvReviewsImports.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 
+import '../../../../../general/constants/MyColors.dart';
 import '../../../../../general/widgets/DefaultAppBar.dart';
+import '../../../../../user/screens/doctor_details/widgets/DoctorDetailsWidgetsImports.dart';
 
 part 'ProvReviews.dart';
 part 'ProvReviewsData.dart';
