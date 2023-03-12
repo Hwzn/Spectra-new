7578704835a699317b4216e4503d7c7698067ff6
AyaHamed1 <ayah.hamed@hotmail.com>
2023-03-12 19:53:57 +0400

    home design

--- lib/provider/screens/prov_home/tabs/prov_main/ProvMain.dart
+++ lib/provider/screens/prov_home/tabs/prov_main/ProvMain.dart
@@ -23,7 +23,7 @@ class _ProvMainState extends State<ProvMain> {
         ),
         actions: [
           IconButton(
-            onPressed: ()=> AutoRouter.of(context).push(NotificationsRoute()),
+            onPressed: () => AutoRouter.of(context).push(NotificationsRoute()),
             padding: const EdgeInsets.symmetric(horizontal: 20),
             icon: Icon(
               Icons.notifications_rounded,
@@ -33,6 +33,97 @@ class _ProvMainState extends State<ProvMain> {
           ),
         ],
         child: Image.asset(Res.homeLogoWhite, width: 150),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Expanded(
+                child: DefaultContainer(
+                  margin: const EdgeInsets.symmetric(vertical: 15),
+                  padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      MyText(
+                        title: "70",
+                        size: 30,
+                        color: MyColors.black,
+                        fontFamily: GoogleFonts.poppins().fontFamily,
+                      ),
+                      const SizedBox(height: 10),
+                      MyText(
+                        title: "Total \nRatings",
+                        size: 16,
+                        color: MyColors.black,
+                        alien: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(width: 20),
+              Expanded(
+                child: DefaultContainer(
+                  margin: const EdgeInsets.symmetric(vertical: 15),
+                  padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      MyText(
+                        title: "55",
+                        size: 30,
+                        color: MyColors.black,
+                        fontFamily: GoogleFonts.poppins().fontFamily,
+                      ),
+                      const SizedBox(height: 10),
+                      MyText(
+                        title: "Total \nReservations",
+                        size: 16,
+                        color: MyColors.black,
+                        alien: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+          DefaultContainer(
+            margin: const EdgeInsets.symmetric(vertical: 10),
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+            child: Row(
+              children: [
+                Expanded(
+                  child: MyText(
+                    title: "Total Ratings",
+                    size: 16,
+                    color: MyColors.black,
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    MyText(
+                      title: "12,000",
+                      size: 20,
+                      color: MyColors.black,
+                      fontFamily: GoogleFonts.poppins().fontFamily,
+                    ),
+                    MyText(
+                      title: "SAR",
+                      size: 12,
+                      color: MyColors.grey,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/provider/screens/prov_home/tabs/prov_main/ProvMainImports.dart
+++ lib/provider/screens/prov_home/tabs/prov_main/ProvMainImports.dart
@@ -1,10 +1,13 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:hwzn_base/general/widgets/DefaultAppBar.dart';
 import 'package:hwzn_base/res.dart';
 
 import '../../../../../general/constants/MyColors.dart';
 import '../../../../../general/utilities/routers/RouterImports.gr.dart';
+import '../../../../../general/widgets/MyText.dart';
+import '../../../../widgets/DefaultContainer.dart';
 
 part 'ProvMain.dart';
 part 'ProvMainData.dart';
